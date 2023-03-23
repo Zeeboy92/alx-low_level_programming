21ae9d40849e9aa63dd9ab66a2ab170d03d8eaef
Zeeboy92 <zoedavid383@gmail.com>
2023-03-23 09:13:47 -0700

    ok

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,3 +28,21 @@
 		if (bytes < 0)
 		{
 			printf("Error\n");
+			exit(2);
+		}
+
+
+		arr = (char *)main;
+
+
+		for (i = 0; i < bytes; i++)
+		{
+			if (i == bytes - 1)
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
+			}
+			printf("%02hhx ", arr[i]);
+		}
+		return (0);
+	}
